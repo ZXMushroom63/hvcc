e69721dfd559d1b4e854d31c930ddaa755f3c584
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-04-28 15:11:50 +0800

    remove another log flooder

--- hvcc/generators/c2js/template/hv_worklet.js
+++ hvcc/generators/c2js/template/hv_worklet.js
@@ -352,7 +352,7 @@ function sendMidiOut(sendName, msg) {
               [_hv_msg_getFloat(msg, 0), _hv_msg_getFloat(msg, 1)] :
               [_hv_msg_getFloat(msg, 0), _hv_msg_getFloat(msg, 1), _hv_msg_getFloat(msg, 2)];
           default:
-              console.warn(`Unhandled sendName: ${sendName}`);
+              //console.warn(`Unhandled sendName: ${sendName}`);
               return [];
   }
 }
